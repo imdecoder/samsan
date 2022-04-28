c2e8f7f28ffef3abf57393dea43a205a4ba19c2e
Wax <imdecoder@mail.com>
2022-04-28 15:42:21 +0300

    Routes düzenlendi.

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -7,6 +7,11 @@
 		$routes->group('subscribers', function ($routes) {
 			$routes->match(['get', 'post'], '/', 'Definitions\Subscribers::list', ['as' => 'definitions.subscribers']);
 			$routes->match(['get', 'post'], 'add', 'Definitions\Subscribers::add', ['as' => 'definitions.subscribers.add']);
+			$routes->match(['get', 'post'], 'edit/(:any)', 'Definitions\Subscribers::edit', ['as' => 'definitions.subscribers.edit']);
+		});
+
+		$routes->group('rates', function ($routes) {
+			$routes->match(['get', 'post'], '/', 'Definitions\Rates::list', ['as' => 'definitions.rates']);
 		});
 
 	});
