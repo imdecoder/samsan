96b398af45caaecdd20ac98ddcad006c3df1d125
Wax <coderwax@gmail.com>
2022-05-07 19:46:07 +0300

    Routes düzenlendi.

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -7,11 +7,20 @@
 		$routes->group('subscribers', function ($routes) {
 			$routes->match(['get', 'post'], '/', 'Definitions\Subscribers::list', ['as' => 'definitions.subscribers']);
 			$routes->match(['get', 'post'], 'add', 'Definitions\Subscribers::add', ['as' => 'definitions.subscribers.add']);
-			$routes->match(['get', 'post'], 'edit/(:any)', 'Definitions\Subscribers::edit', ['as' => 'definitions.subscribers.edit']);
+			$routes->match(['get', 'post'], 'edit/(:any)', 'Definitions\Subscribers::edit/$1', ['as' => 'definitions.subscribers.edit']);
 		});
 
 		$routes->group('rates', function ($routes) {
 			$routes->match(['get', 'post'], '/', 'Definitions\Rates::list', ['as' => 'definitions.rates']);
+		});
+
+	});
+
+	$routes->group('entries', function ($routes) {
+
+		$routes->group('endexes', function ($routes) {
+			$routes->get('/', 'Entries\Endexes::list', ['as' => 'entries.endexes']);
+			$routes->match(['get', 'post'], 'edit/(:any)', 'Entries\Endexes::edit/$1', ['as' => 'entries.endexes.edit']);
 		});
 
 	});
